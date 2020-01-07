34a3aeb9c2c351d1692f4c85db26a942befddec0
Eduardo Imery <ejwimery@gmail.com>
2020-01-07 15:01:46 -0600

    creating the xpath for enter to instagram account

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
-
-#browser = webdriver.Chrome('/Users/k4is3r/Desktop/seleniumChrome/chromedriver')
+from secrets import USERNAME,PW
+import time 
 
 class InstaBot:
-    def __init__(self):
+    def __init__(self, username, pw):
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument('--incognito')
         self.driver = webdriver.Chrome('./chromedriver',options=self.chrome_options)
-        self.driver.get('https://instagram.com') 
-
+        self.driver.get('https://instagram.com')
+        time.sleep(5)
+        self.driver.find_element_by_xpath('//a[contains(text(), "Inicia sesión")]').click()
+        time.sleep(5) 
+        self.driver.find_element_by_xpath('//input[@name=\'username\']').send_keys(username)
+        self.driver.find_element_by_xpath('//input[@name=\'password\']').send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
+        time.sleep(5)
 
 if __name__ == '__main__':
-    InstaBot()
+    InstaBot(USERNAME,PW)
