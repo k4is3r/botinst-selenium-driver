32d4841db1d9f70c0f320fef72b596ec02a8439f
Eduardo Imery <ejwimery@gmail.com>
2020-01-07 16:54:03 -0600

    getting to scroll in the follwers box

--- main.py
+++ main.py
@@ -19,10 +19,22 @@ class InstaBot:
         self.driver.find_element_by_xpath('//button[contains(text(),"Ahora no")]').click()
         time.sleep(6)
         
-    def get_unfollowers(self):
-        self.driver.find_element_by_xpath('//a[contains(@href,"/{}")]'.format(self.username)).cli        
+            
     def get_unfollowers(self):
         self.driver.find_element_by_xpath('//a[contains(@href,"/{}")]'.format(self.username)).click()
+        self.driver.find_element_by_xpath('//a[contains(@href, "/seguidores")]')\
+                   .click()
+        sugs =self.driver.find_element_by_xpath('//h4[contains(text(),Suggestions)]')
+        self.driver.execute_scripts('arguments[0].scrollIntoView()',sugs)
+        scroll_box = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[2]')
+        last_h, ht = 0, 1
+        while last_h != ht:
+            last_h = ht
+            time.sleep(3)
+            ht = self.driver.execute_scripts("""
+                 arguments[0].scrollTo(0, arguments[0].scrollHeight);
+                 return arguments[0].scrollHeight;
+                 """, scroll_box)
 
 
 if __name__ == '__main__':
